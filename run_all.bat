6034bbf1d2a2b55d39ea5282535f9f7a26849cd8
RashadShamil <SHAMILRASHAD@GMAIL.COM>
2026-09-25 19:37:33 +0530

    pyc files add

--- run_all.bat
+++ run_all.bat
@@ -12,5 +12,10 @@ echo Starting Frontend (Flutter Windows)...
 start "OptiFlow Frontend" cmd /k "cd optiflow_front && flutter run -d windows"
 
 echo.
+echo Starting Frontend (Flutter Android)...
+start "OptiFlow Frontend" cmd /k "cd optiflow_front && flutter run"
+
+
+echo.
 echo Both services have been launched in separate terminal windows!
 echo Close this window at any time.
